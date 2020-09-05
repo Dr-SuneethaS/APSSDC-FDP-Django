ef48094499d1c5b8b247b27e6b9b1adfb0795507
vijay <Vijay10022@gmail.com>
2020-09-05 11:03:58 +0530

    Day-6

---
+++ Day6/bootapp/myapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def register(request):
+	if request.method=="POST":
+		f_name=request.POST['f_name']
+		l_name=request.POST['l_name']
+		gmail=request.POST['gmail']
+		phn=request.POST['ph_no']
+		username=request.POST['username']
+		password=request.POST['password']
+		data={
+			'first_name':f_name,
+			'last_name':l_name,
+			'Gmail':gmail,
+			'phone_number':phn,
+			'username':username,
+			'password':password
+		}
+		# print(f_name,l_name,gmail,phn,username,password)
+		return render(request,'mytemplates/mydata.html',data)
+
+
+	return render(request,'mytemplates/register.html')
+def login(request):
+	return render(request,'mytemplates/login.html')
